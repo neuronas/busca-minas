8e974e77b69a61bf061143ec19693d921bfc1afb
orlando <orlandonuske@gmail.com>
2017-12-29 14:35:47 -0300

    fix CORS request y la funcion grid recibe parametros x y

--- api/app.js
+++ api/app.js
@@ -11,27 +11,44 @@ app.use(methodOverride());
 
 var router = express.Router();
 
+
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+
+
+
 router.get('/', function(req, res) {
    res.send("success");
 });
 
 
-router.get('/grid', function(req, res) {
+
+
+router.post('/grid', function(req, result) {
+  // console.log("REQ--1", req.headers)
+  console.log("REQ--2", req.body)
+
+  var params = req.body
 
   var matrix = []
 
-  grid.build(10, 10, function(res){
-    console.log("res", res)
+  grid.build(req.body.sizeX, req.body.sizeY, function(res){
+    // console.log("res", res)
     matrix = res
 
     grid.setMines(matrix, function(res2){
-      console.log("res2", res2)
+      var res2 = JSON.stringify(res2)
+      // console.log("res2", res2)
+      result.send(res2);
     })
 
   })
 
 
-   res.send("grid");
 });
 
 router.post('/open', function(req, res) {
