a3003eb0dbb7bb8777fc48596feeefe28476d22e
orlando <orlandonuske@gmail.com>
2018-01-10 00:46:37 -0300

    test disable cors

--- api/app.js
+++ api/app.js
@@ -12,11 +12,11 @@ app.use(methodOverride());
 var router = express.Router();
 
 
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
+// app.all('/*', function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+//   next();
+// });
 
 
 
